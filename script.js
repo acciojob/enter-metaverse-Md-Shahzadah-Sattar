584836171dd50a8f7020d53604ab1b50d8286630
Md-Shahzadah-Sattar <148210694+Md-Shahzadah-Sattar@users.noreply.github.com>
2023-12-29 23:22:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+    // Find the elements by their IDs
+    const statusElement = document.getElementById('status');
+    const enterBtn = document.getElementById('enterBtn');
+
+    // Add a click event listener to the button
+    enterBtn.addEventListener('click', function () {
+        // Change the text content of the status element
+        statusElement.textContent = 'Entered Metaverse';
+
+        // Optionally, create and append an h1 element
+        const h1Element = document.createElement('h1');
+        h1Element.textContent = 'Entered Metaverse';
+        document.body.appendChild(h1Element);
+    });
+});
